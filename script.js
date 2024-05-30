cccc457c9048519e74e402635be61f7c613eb1c5
kodan96 <farrkas53@gmail.com>
2024-05-31 00:20:12 +0200

    home page, careers page, about page + locations mobile layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-
+    const questions = document.querySelectorAll('.faq_item');
 
     const menuToggle = () => {
         $('.open').toggle();
@@ -7,6 +7,19 @@ $(document).ready(() => {
         $('nav').slideToggle();
         $('.page_overlay').fadeToggle();
     }
+
+    
+    const answerToggle = (i) => {
+        $('.faq_item--answer').eq(i).slideToggle();
+        let img = $('.faq_item--question img').eq(i);
+        if (img.css('transform') === 'none' || img.css('transform') === 'matrix(1, 0, 0, 1, 0, 0)') {
+            img.css('transform', 'rotate(180deg)');
+        } else {
+            img.css('transform', 'rotate(0deg)');
+        }
+    };
+        
+    
 
 
 
@@ -18,5 +31,12 @@ $(document).ready(() => {
 
     $('.open, .close').on('click', () => {
         menuToggle();
+    })
+
+    questions.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            answerToggle(i);
+            
+        })
     })
 })
